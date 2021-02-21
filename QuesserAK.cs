7b2f31847a66df5d0436ab06134c9dcb2032356d
Anthon <Anthon>
2021-02-21 19:05:34 +0100

    Added File

---
+++ QuesserAK.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Guesser_exercise
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter name. You have 3 tries. First letter must be capital");
+            
+            int Tries = 3;
+
+            string CorrectName = "Allan";
+
+            bool activeAccess = false;
+
+
+
+            while (activeAccess == false)
+            {
+
+                String EnteredName = Console.ReadLine();
+
+                if (EnteredName == CorrectName)
+                {
+
+                    activeAccess = true;
+                    Console.WriteLine("Correct. Access granted");
+
+                }
+
+                else if (EnteredName != CorrectName)
+                {
+                    Tries--;
+                    if(Tries >= 0)
+                    {
+                        Console.WriteLine("Incorrect. Tries left is " + Tries);
+                    }
+                    
+
+
+                    if (Tries == 0)
+                    {
+                        Console.WriteLine("Initiate self-destruct sequence ");
+                        activeAccess = true;
+                    }
+                    
+                }
+               
+
+
+
+            }
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
